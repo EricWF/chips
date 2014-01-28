aa3079d9a9e9bbddd90e94d97f342733d5b20495
Eric Fiselier <eric@efcs.ca>
2014-01-28 13:16:20 -0700

    c++1y test to check for stdlib compat

---
+++ c++1y_test.cpp
@@ -0,0 +1,93 @@
+# include <cstdlib>
+# include <csignal>
+# include <csetjmp>
+# include <cstdarg>
+# include <typeinfo>
+# include <typeindex>
+# include <type_traits>
+# include <bitset>
+# include <functional>
+# include <utility>
+# include <ctime>
+# include <chrono>
+# include <cstddef>
+# include <initializer_list>
+
+# include <tuple>
+# include <new>
+# include <memory>
+# include <scoped_allocator>
+
+# include <climits>
+# include <cfloat>
+# include <limits>
+
+# include <exception>
+# include <stdexcept>
+# include <cassert>
+# include <system_error>
+# include <cerrno>
+
+# include <cctype>
+# include <cwctype>
+# include <cstring>
+# include <cwchar>
+//# include <cuchar>
+# include <string>
+
+# include <array>
+# include <vector>
+# include <deque>
+# include <list>
+# include <forward_list>
+# include <set>
+# include <map>
+# include <unordered_set>
+# include <unordered_map>
+# include <stack>
+# include <queue>
+
+# include <algorithm>
+
+# include <iterator>
+
+# include <cmath>
+# include <complex>
+# include <valarray>
+# include <random>
+# include <numeric>
+# include <ratio>
+# include <cfenv>
+
+# include <iosfwd>
+# include <ios>
+# include <istream>
+# include <ostream>
+# include <iostream>
+# include <fstream>
+# include <sstream>
+# include <strstream>
+# include <iomanip>
+# include <streambuf>
+# include <cstdio>
+
+# include <locale>
+# include <clocale>
+# include <codecvt>
+
+# include <regex>
+
+# include <thread>
+# include <mutex>
+// # include <shared_mutex>
+# include <future>
+# include <condition_variable>
+
+# include <ccomplex>
+# include <ctgmath>
+//# include <cstdalign>
+# include <cstdbool>
+
+int main() {}
+
+
