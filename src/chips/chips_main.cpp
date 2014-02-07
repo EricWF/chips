277cafe2c17046d57fce86f4e5b924a7590f046f
carson <tunna.carson@gmail.com>
2014-02-07 13:27:11 -0700

    Need to add -lSDL2_image flag to linker

--- src/chips/chips_main.cpp
+++ src/chips/chips_main.cpp
@@ -2,410 +2,129 @@
 #include "chips/chips_main.hpp"
 
 #include <SDL2/SDL.h>
-#include <SDL/SDL_image.h>
-#include <stdio.h>
+#include <SDL2/SDL_image.h>
 #include <string>
+#include <iostream>
 
-//The screen attributes
-const int SCREEN_WIDTH = 640;
+const int SCREEN_WIDTH  = 640;
 const int SCREEN_HEIGHT = 480;
-const int SCREEN_BPP = 32;
+const int TILE_SIZE = 40;
 
-//The frame rate
-const int FRAMES_PER_SECOND = 20;
-
-//The dimensions of the dot
-const int DOT_WIDTH = 20;
-const int DOT_HEIGHT = 20;
-
-//The surfaces
-SDL_Surface *dot = nullptr;
-SDL_Surface *screen = nullptr;
-
-//The event structure
-SDL_Event event;
-
-
-
-//The dot that will move around on the screen
-class Dot
-{
-private:
-  //The X and Y offsets of the dot
-  int x, y;
-
-  //The velocity of the dot
-  int xVel, yVel;
-
-public:
-  //Initializes the variables
-  Dot();
-
-  //Takes key presses and adjusts the dot's velocity
-  void handle_input();
-
-  //Moves the dot
-  void move();
-
-  //Shows the dot on the screen
-  void show();
-};
-
-//The timer
-class Timer
-{
-private:
-  //The clock time when the timer started
-  int startTicks;
-
-  //The ticks stored when the timer was paused
-  int pausedTicks;
-
-  //The timer status
-  bool paused;
-  bool started;
-
-public:
-  //Initializes variables
-  Timer();
-
-  //The various clock actions
-  void start();
-  void stop();
-  void pause();
-  void unpause();
-
-  //Gets the timer's time
-  int get_ticks();
-
-  //Checks the status of the timer
-  bool is_started();
-  bool is_paused();
-};
-
-SDL_Surface *load_image( std::string filename )
-{
-  //The image that's loaded
-  SDL_Surface* loadedImage = nullptr;
-
-  //The optimized surface that will be used
-  SDL_Surface* optimizedImage = nullptr;
-
-  //Load the image
-  loadedImage = IMG_Load( filename.c_str() );
-
-  //If the image loaded
-  if( !loadedImage )
-    {
-      //Create an optimized surface
-      optimizedImage = SDL_DisplayFormat( loadedImage );
-
-      //Free the old surface
-      SDL_FreeSurface( loadedImage );
-
-      //If the surface was optimized
-      if( !optimizedImage )
-        {
-          //Color key surface
-          SDL_SetColorKey( optimizedImage, SDL_SRCCOLORKEY,
-                           SDL_MapRGB( optimizedImage->format, 0,
-                                       0xFF, 0xFF ) );
-        }
-    }
-
-  //Return the optimized surface
-  return optimizedImage;
+/**
+ * Log an SDL error with some error message to the output stream of our choice
+ * @param os The output stream to write the message too
+ * @param msg The error message to write, format will be msg error: SDL_GetError()
+ */
+void logSDLError(std::ostream &os, const std::string &msg){
+  os << msg << " error: " << SDL_GetError() << std::endl;
+}
+/**
+ * Loads an image into a texture on the rendering device
+ * @param file The image file to load
+ * @param ren The renderer to load the texture onto
+ * @return the loaded texture, or nullptr if something went wrong.
+ */
+SDL_Texture* loadTexture(const std::string &file, SDL_Renderer *ren){
+  SDL_Texture *texture = IMG_LoadTexture(ren, file.c_str());
+  if (texture == nullptr)		
+    logSDLError(std::cout, "LoadTexture");
+  return texture;
+}
+/**
+ * Draw an SDL_Texture to an SDL_Renderer at position x, y, with some desired
+ * width and height
+ * @param tex The source texture we want to draw
+ * @param ren The renderer we want to draw too
+ * @param x The x coordinate to draw too
+ * @param y The y coordinate to draw too
+ * @param w The width of the texture to draw
+ * @param h The height of the texture to draw
+ */
+void renderTexture(SDL_Texture *tex, SDL_Renderer *ren, int x, int y, int w, int h){
+  //Setup the destination rectangle to be at the position we want
+  SDL_Rect dst;
+  dst.x = x;
+  dst.y = y;
+  dst.w = w;
+  dst.h = h;
+  SDL_RenderCopy(ren, tex, NULL, &dst);
+}
+/**
+ * Draw an SDL_Texture to an SDL_Renderer at position x, y, preserving
+ * the texture's width and height
+ * @param tex The source texture we want to draw
+ * @param ren The renderer we want to draw too
+ * @param x The x coordinate to draw too
+ * @param y The y coordinate to draw too
+ */
+void renderTexture(SDL_Texture *tex, SDL_Renderer *ren, int x, int y){
+  int w, h;
+  SDL_QueryTexture(tex, NULL, NULL, &w, &h);
+  renderTexture(tex, ren, x, y, w, h);
 }
 
-void apply_surface( int x, int y, SDL_Surface* source,
-                    SDL_Surface* destination, SDL_Rect* clip = nullptr )
-{
-  //Holds offsets
-  SDL_Rect offset;
 
-  //Get offsets
-  offset.x = x;
-  offset.y = y;
+int chips_main(int, char**, char**){
+  //Start up SDL and make sure it went ok
+  if (SDL_Init(SDL_INIT_EVERYTHING) != 0){
+    logSDLError(std::cout, "SDL_Init");
+    return 1;
+  }
 
-  //Blit
-  SDL_BlitSurface( source, clip, destination, &offset );
-}
+  //Setup our window and renderer
+  SDL_Window *window = SDL_CreateWindow("Lesson 3", 100, 100, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+  if (window == nullptr){
+    logSDLError(std::cout, "CreateWindow");
+    return 2;
+  }
+  SDL_Renderer *renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+  if (renderer == nullptr){
+    logSDLError(std::cout, "CreateRenderer");
+    return 3;
+  }
+	
+  //The textures we'll be using
+  SDL_Texture *background = loadTexture("../res/Lesson3/background.png", renderer);
+  SDL_Texture *image = loadTexture("../res/Lesson3/image.png", renderer);
+  //Make sure they both loaded ok
+  if (background == nullptr || image == nullptr)
+    return 4;
 
-bool init()
-{
-  //Initialize all SDL subsystems
-  if( SDL_Init( SDL_INIT_EVERYTHING ) == -1 )
-    {
-      return false;
-    }
+  //Clear the window
+  SDL_RenderClear(renderer);
 
-  //Set up the screen
-  screen = SDL_SetVideoMode( SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_BPP, SDL_SWSURFACE );
+  //Determine how many tiles we'll need to fill the screen
+  int xTiles = SCREEN_WIDTH / TILE_SIZE;
+  int yTiles = SCREEN_HEIGHT / TILE_SIZE;
 
-  //If there was an error in setting up the screen
-  if( screen == NULL )
-    {
-      return false;
-    }
+  //Draw the tiles by calculating their positions
+  for (int i = 0; i < xTiles * yTiles; ++i){
+    int x = i % xTiles;
+    int y = i / xTiles;
+    renderTexture(background, renderer, x * TILE_SIZE, y * TILE_SIZE, TILE_SIZE, TILE_SIZE);
+  }
 
-  //Set the window caption
-  SDL_WM_SetCaption( "Move the Dot", NULL );
+  //Draw our image in the center of the window
+  //We need the foreground image's width to properly compute the position
+  //of it's top left corner so that the image will be centered
+  int iW, iH;
+  SDL_QueryTexture(image, NULL, NULL, &iW, &iH);
+  int x = SCREEN_WIDTH / 2 - iW / 2;
+  int y = SCREEN_HEIGHT / 2 - iH / 2;
+  renderTexture(image, renderer, x, y);
 
-  //If everything initialized fine
-  return true;
-}
+  //Update the screen
+  SDL_RenderPresent(renderer);
+  SDL_Delay(2000);
 
-bool load_files()
-{
-  //Load the dot image
-  dot = load_image( "dot.bmp" );
+  //Destroy the various items
+  SDL_DestroyTexture(background);
+  SDL_DestroyTexture(image);
+  SDL_DestroyRenderer(renderer);
+  SDL_DestroyWindow(window);
 
-  //If there was a problem in loading the dot
-  if( dot == NULL )
-    {
-      return false;
-    }
-
-  //If everything loaded fine
-  return true;
-}
-
-void clean_up()
-{
-  //Free the surface
-  SDL_FreeSurface( dot );
-
-  //Quit SDL
+  IMG_Quit();
   SDL_Quit();
-}
-
-Dot::Dot()
-{
-  //Initialize the offsets
-  x = 0;
-  y = 0;
-
-  //Initialize the velocity
-  xVel = 0;
-  yVel = 0;
-}
-
-void Dot::handle_input()
-{
-  //If a key was pressed
-  if( event.type == SDL_KEYDOWN )
-    {
-      //Adjust the velocity
-      switch( event.key.keysym.sym )
-        {
-        case SDLK_UP: yVel -= DOT_HEIGHT / 2; break;
-        case SDLK_DOWN: yVel += DOT_HEIGHT / 2; break;
-        case SDLK_LEFT: xVel -= DOT_WIDTH / 2; break;
-        case SDLK_RIGHT: xVel += DOT_WIDTH / 2; break;
-        }
-    }
-  //If a key was released
-  else if( event.type == SDL_KEYUP )
-    {
-      //Adjust the velocity
-      switch( event.key.keysym.sym )
-        {
-        case SDLK_UP: yVel += DOT_HEIGHT / 2; break;
-        case SDLK_DOWN: yVel -= DOT_HEIGHT / 2; break;
-        case SDLK_LEFT: xVel += DOT_WIDTH / 2; break;
-        case SDLK_RIGHT: xVel -= DOT_WIDTH / 2; break;
-        }
-    }
-}
-
-void Dot::move()
-{
-  //Move the dot left or right
-  x += xVel;
-
-  //If the dot went too far to the left or right
-  if( ( x < 0 ) || ( x + DOT_WIDTH > SCREEN_WIDTH ) )
-    {
-      //move back
-      x -= xVel;
-    }
-
-  //Move the dot up or down
-  y += yVel;
-
-  //If the dot went too far up or down
-  if( ( y < 0 ) || ( y + DOT_HEIGHT > SCREEN_HEIGHT ) )
-    {
-      //move back
-      y -= yVel;
-    }
-}
-
-void Dot::show()
-{
-  //Show the dot
-  apply_surface( x, y, dot, screen );
-}
-
-Timer::Timer()
-{
-  //Initialize the variables
-  startTicks = 0;
-  pausedTicks = 0;
-  paused = false;
-  started = false;
-}
-
-void Timer::start()
-{
-  //Start the timer
-  started = true;
-
-  //Unpause the timer
-  paused = false;
-
-  //Get the current clock time
-  startTicks = SDL_GetTicks();
-}
-
-void Timer::stop()
-{
-  //Stop the timer
-  started = false;
-
-  //Unpause the timer
-  paused = false;
-}
-
-void Timer::pause()
-{
-  //If the timer is running and isn't already paused
-  if( ( started == true ) && ( paused == false ) )
-    {
-      //Pause the timer
-      paused = true;
-
-      //Calculate the paused ticks
-      pausedTicks = SDL_GetTicks() - startTicks;
-    }
-}
-
-void Timer::unpause()
-{
-  //If the timer is paused
-  if( paused == true )
-    {
-      //Unpause the timer
-      paused = false;
-
-      //Reset the starting ticks
-      startTicks = SDL_GetTicks() - pausedTicks;
-
-      //Reset the paused ticks
-      pausedTicks = 0;
-    }
-}
-
-int Timer::get_ticks()
-{
-  //If the timer is running
-  if( started == true )
-    {
-      //If the timer is paused
-      if( paused == true )
-        {
-          //Return the number of ticks when the timer was paused
-          return pausedTicks;
-        }
-      else
-        {
-          //Return the current time minus the start time
-          return SDL_GetTicks() - startTicks;
-        }
-    }
-
-  //If the timer isn't running
-  return 0;
-}
-
-bool Timer::is_started()
-{
-  return started;
-}
-
-bool Timer::is_paused()
-{
-  return paused;
-}
-
-
-int chips_main(int, char**, char**)
-{
-  //Quit flag
-  bool quit = false;
-
-  //The dot that will be used
-  Dot myDot;
-
-  //The frame rate regulator
-  Timer fps;
-
-  //Initialize
-  if( init() == false )
-    {
-      return 1;
-    }
-
-  //Load the files
-  if( load_files() == false )
-    {
-      return 1;
-    }
-
-  //While the user hasn't quit
-  while( quit == false )
-    {
-      //Start the frame timer
-      fps.start();
-
-      //While there's events to handle
-      while( SDL_PollEvent( &event ) )
-        {
-          //Handle events for the dot
-          myDot.handle_input();
-
-          //If the user has Xed out the window
-          if( event.type == SDL_QUIT )
-            {
-              //Quit the program
-              quit = true;
-            }
-        }
-
-      //Move the dot
-      myDot.move();
-
-      //Fill the screen white
-      SDL_FillRect( screen, &screen->clip_rect, SDL_MapRGB( screen->format, 0xFF, 0xFF, 0xFF ) );
-
-      //Show the dot on the screen
-      myDot.show();
-
-      //Update the screen
-      if( SDL_Flip( screen ) == -1 )
-        {
-          return 1;
-        }
-
-      //Cap the frame rate
-      if( fps.get_ticks() < 1000 / FRAMES_PER_SECOND )
-        {
-          SDL_Delay( ( 1000 / FRAMES_PER_SECOND ) - fps.get_ticks() );
-        }
-    }
-
-  //Clean up
-  clean_up();
-
+	
   return 0;
 }
