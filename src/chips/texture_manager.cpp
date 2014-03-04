5d289339f1108479cb81627c0e47b345a1b4a3dd
Eric Fiselier <eric@efcs.ca>
2014-03-03 19:53:58 -0700

    fixed masking for tileset

--- src/chips/texture_manager.cpp
+++ src/chips/texture_manager.cpp
@@ -4,6 +4,30 @@
 
 namespace chips
 {
+    
+    namespace detail
+    {
+        inline void mask_tileset(sf::Image & img)
+        {
+            const unsigned cutout_start_col = 7;
+            const unsigned cutout_start_xpos = cutout_start_col * tile_width;
+            const unsigned mask_start_col = 10;
+            const unsigned mask_start_xpos = mask_start_col * tile_width;
+            
+            const unsigned mask_width = tile_width * 3;
+            const unsigned mask_height = texture_height;
+            
+            for (unsigned x=0; x < mask_width; ++x)
+            {
+                for (unsigned y=0; y < mask_height; ++y)
+                {
+                    sf::Color mask_c = img.getPixel(x + mask_start_xpos, y);
+                    if (mask_c == sf::Color::Black)
+                        img.setPixel(x + cutout_start_xpos, y, sf::Color::Transparent);
+                }
+            }
+        }
+    }                                                       // namespace detail
 #if defined(__clang__)
 # pragma clang diagnostic push
 # pragma clang diagnostic ignored "-Wexit-time-destructors"
@@ -23,8 +47,7 @@ namespace chips
         if (!img.loadFromFile(CHIPS_RESOURCE_ROOT "/tileset.gif"))
             throw "TODO";
         
-        sf::Color mask(255, 255, 255, 255);
-        img.createMaskFromColor(mask);
+        detail::mask_tileset(img);
        
         if (!m_tex_map[texture_uid::tiles].loadFromImage(img))
             throw "TODO";
