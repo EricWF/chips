0ac7d9bae9b1a822f2adb9a763291cd718f7fd09
Eric Fiselier <eric@efcs.ca>
2014-04-09 21:11:15 -0600

    fixed double move bug in chips

--- src/chips/logic/chip.cpp
+++ src/chips/logic/chip.cpp
@@ -12,36 +12,9 @@ namespace chips { namespace logic
         ELIB_ASSERT(is_chip(e));
         e.remove<tile_id>();
         
-        auto chip_update_ =
-        [](entity & self, level & l)
-        {
-            auto end = l.entity_list.end();
-            
-            if (!self.get<inventory>().contains(entity_id::skates))
-            {
-                auto ice_pos = OnIce(self).find(l.entity_list);
-                if (ice_pos != end)
-                {
-                    move_on_ice(self, *ice_pos, l);
-                    return;
-                }
-            }
-            
-            if (!self.get<inventory>().contains(entity_id::suction_boots))
-            {
-                auto ff_pos = OnForceFloor(self).find(l.entity_list);
-                if (ff_pos != end)
-                {
-                    move_on_force_floor(self, *ff_pos, l);
-                    return;
-                }
-            }
-        };
-        
         e << inventory() 
-          << method(update_, chip_update_)
-          << method(move_, common::move_)
-          << regular_speed;
+          << method(move_, common::move_);
+          
     }
     
     void process_chip(entity &, level &)
